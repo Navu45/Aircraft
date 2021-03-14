851a0fa4e00812a5af001a025f33d05a920f01d8
Alexey <jamoncheryev1@gmail.com>
2021-03-14 19:44:46 +0300

    Add: Airplane model and movements on the ground
    Now we can control airplane movements on the ground. Also it can be accelerated and moves faster.
    Added brakes and friction to it (wheels and drag) for realistic movements. Added Cinemachine Virtual camera to follow the airplane. Added Colliders.
    Added .gitignore and .gitattributes.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public List<WheelCollider> wheels;
+    // Air
+    public float thrust;
+    // Ground
+    public float acceleration;
+
+    [Range(-1, 1)]
+    public float pitch;
+    [Range(-1, 1)]
+    public float roll;
+    [Range(0, 1)]
+    public float brake;
+    [Range(0, 1)]
+    public float flap;
+    [Range(1, 1)]
+    public float yaw;
+
+    public bool isFlying;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isFlying = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isFlying)
+        {
+            foreach (var wheel in wheels)
+            {
+                wheel.motorTorque = 0.01f;
+            }
+            isFlying = MovingOnGround();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pitch = Input.GetAxis("Vertical");
+        roll = Input.GetAxis("Horizontal");
+        yaw = Input.GetAxis("Yaw");
+        brake = Input.GetAxis("Jump");
+    }
+
+    bool MovingOnGround()
+    {
+        if (pitch != 0)
+        {
+            rb.AddRelativeForce(Vector3.forward * thrust * pitch * (Input.GetKey(KeyCode.U) ? acceleration : 1)); // U is acceleration
+            rb.AddRelativeTorque(Vector3.up * thrust  * roll * (rb.velocity.magnitude > 2 && !Input.GetKey(KeyCode.U) ? 1 : 0));
+        }
+
+        if (brake != 0)
+        {
+            rb.drag = rb.velocity.magnitude < 0.7 ? 1 : 0.7f;
+        }
+        else rb.drag = 0.3f;
+
+        if (rb.velocity.magnitude > 30f)
+        {
+            rb.drag = 0;
+            rb.angularDrag = 0.05f;
+            return true;
+        }
+        return false;
+    }
+}
